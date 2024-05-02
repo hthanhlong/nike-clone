6982e8b30d28581572dcb29543fcf37edf63f225
hthanhlong <hthanhlong97@gmail.com>
2024-05-02 12:13:01 -0700

    fix eslint:

--- src/pages/product/ProductDetail.tsx
+++ src/pages/product/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Modal } from 'flowbite-react'
+import { Modal } from 'flowbite-react'
 import { image1, image2, image3, image4 } from '../../assets/images'
 import { Zoom } from '../../components'
 
