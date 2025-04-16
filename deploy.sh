b4656be9ffb5c7ccc0ff6e9efc86c6192ccc0e7a
hthanhlong <hthanhlong97@gmail.com>
2025-04-15 22:38:34 -0700

    edit dockerfile

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# stop the container
+docker stop nike_clone
+
+# remove the container
+docker rm nike_clone
+
+# remove the image
+docker rmi nike_clone
+
+# build the image
+docker build -t nike_clone .
+
+# run the container
+docker run -d --name nike_clone -p 80:80 nike_clone
